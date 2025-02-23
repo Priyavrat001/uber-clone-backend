3966b1302b8ae9b223a14a895952b71ee6222d69
Priyavrat <priyevratsharma@gmail.com>
2025-02-23 20:15:46 +0530

    Added dublicate key error in middleware

--- middleware/error.js
+++ middleware/error.js
@@ -2,6 +2,12 @@ const errorMiddleware = (err, req, res, next) => {
   
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
+
+    if (err.code === 11000) {
+        statusCode = 400;
+        const field = Object.keys(err.keyValue)[0];
+        message = `Duplicate value entered for ${field} field. Please use another value.`;
+      }
   
     res.status(statusCode).json({
       success: false,
